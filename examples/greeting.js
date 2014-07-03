3dc40513646eb227efb49ececa280111dfb25498
Schmoopiie <Schmoopiie@gmail.com>
2014-07-03 08:44:30 -0400

    Updated greeting.

--- examples/greeting.js
+++ examples/greeting.js
@@ -1,11 +1,15 @@
 /**
  * This is an example of how to use node-twitch-irc.
  * 
+ * Will send greeting to mods. I recommend using a list of mods in your code. The other way would
+ * be to listen to the MODE event, but Twitch IRC servers are very slow.
+ * 
  * You will need to create an account for your bot and retrieve the
  * oauth password (http://twitchapps.com/tmi/).
  */
 
-var irc = require('node-twitch-irc');
+var irc = require('node-twitch-irc'),
+	_ = require('underscore'); // Don't forget to add underscore in your dependencies.
 
 var config = {
 	nickname: 'bot_username',
@@ -13,6 +17,9 @@ var config = {
 	oauth: 'oauth:your_oauth_token',
 	twitchclient: 1
 };
+
+// List of mods in your channel.
+var mods = ['username1','username2','username3','username4']
 
 var client = new irc.connect(config, function(err, event) {
 	if (!err) {
@@ -37,7 +44,9 @@ var client = new irc.connect(config, function(err, event) {
 		// "Join" event.
 		event.on("join", function (channel, username) {
 			console.log(username+' JOINED '+channel);
-			if (username !== config.nickname) {
+			
+			// username is in 'mods'
+			if (_.indexOf(mods, username) >= 0) {
 				client.say(channel, 'Welcome, '+username+'!');
 			}
 		});
